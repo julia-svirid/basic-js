885947f111d6f04770ad4df0e77c1b49a3ae898c
julia-svirid <yulia1903@gmail.com>
2020-03-10 15:40:49 +0300

    feat: add solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,18 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let word = "";
+  for (let i = 0; i<members.length; i++){
+    if(typeof members[i] == "string"){
+      word = word + members[i].slice(0, 1);
+    }
+    else return false;
+  }
+  
+  let str = word.toUpperCase();
+  let arr = str.split("");
+  let sortedarr = arr.sort();
+  let result = sortedarr.join('');
+ 
+
+  return result;
+  
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,21 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) throw Error;
+    for (let i = 0; i<arr.length; i++){
+        if (arr[i]=="--double-next") {
+          arr[i]=arr[i+1];
+        }
+    
+        if (arr[i]=="--double-prev") {
+          arr[i]=arr[i-1];
+        }
+    
+        if (arr[i]=="--discard-next"){
+          arr.splice(i,2);
+        }
+    
+        if (arr[i]=="--discard-prev"){
+          arr.splice(i-1,2);
+        }
+      }
+      return arr;
 };
